5c9fb22a9ada16526a7677f462de769642e404d7
mwmdgmb <sadboy7m@gmail.com>
2020-04-06 14:43:37 +0430

    Updates

---
+++ precache-manifest.8d913e9e5677bd1abd5529e816898c28.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f8c998f7bb74e3f539c034cae8d3e058",
+    "url": "/SearchMonster/index.html"
+  },
+  {
+    "revision": "b10f204968dd449ff03f",
+    "url": "/SearchMonster/static/css/2.de424728.chunk.css"
+  },
+  {
+    "revision": "bd67654bcd44fb55e9bc",
+    "url": "/SearchMonster/static/css/main.d1e1d396.chunk.css"
+  },
+  {
+    "revision": "b10f204968dd449ff03f",
+    "url": "/SearchMonster/static/js/2.b985c762.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/SearchMonster/static/js/2.b985c762.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "bd67654bcd44fb55e9bc",
+    "url": "/SearchMonster/static/js/main.a26a5d3c.chunk.js"
+  },
+  {
+    "revision": "83eb17f2cdf6a756c298",
+    "url": "/SearchMonster/static/js/runtime-main.4b23b7b2.js"
+  }
+]);
